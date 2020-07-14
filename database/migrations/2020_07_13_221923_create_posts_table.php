5bce17fdac85178183c393d9331b7025c1fdbf24
eml0777us <eml0777us@gmail.com>
2020-07-14 00:14:53 +0000

    create Posts table and Model

--- database/migrations/2020_07_13_221923_create_posts_table.php
+++ database/migrations/2020_07_13_221923_create_posts_table.php
@@ -15,6 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('slug')->unique();  // 將標題轉化為 URL 的一部份，有利於 SEO
+            $table->string('title');
+            $table->text('content');
+            $table->softDeletes();
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
