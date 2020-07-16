8e131845534c68386b459e9fc2de090ab0ace370
EmL0777 <eml0777.chen@gmail.com>
2020-07-16 11:13:16 +0800

    create tag & post_tag_pivot migration

---
+++ database/migrations/2020_07_16_030143_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tag')->unique();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('page_image');  // Tag image
+            $table->string('meta_description');  // Tag description
+            $table->string('layout')->default('blog.layouts.index');  // blog's layout
+            $table->string('reverse_direction');  // 文章列表按時間升序 blog 文章 (default 降序)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
